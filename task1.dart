0f3ac7c5138f9852b145d88ed1f5b107f27eeef4
Ahmedbakr72 <bakr53217@gmail.com>
2025-05-31 10:11:17 +0300

    Add files via upload

---
+++ task1.dart
@@ -0,0 +1,67 @@
+
+
+void main(){
+  int age=22;
+  print(age);
+
+  String firstName="ahmed";
+  String lastName="bakr";
+  print(firstName+" "+lastName);
+
+
+  double heightinMeters=172;
+  print(heightinMeters);
+
+
+  bool isStudent=true;
+  isStudent=false;
+  print(isStudent);
+
+
+int Age=22;
+if(age>18){
+  print("Adult");
+}else {
+  print("Minor");
+}
+
+
+int number=22;
+if(number%2==0) {
+  print("even");
+}else {
+  print("odd");
+}
+
+
+int a=10;
+int b=20;
+if(a>b){
+  print("a Greater than b");
+}else{
+  print("b Greater than a");
+}
+
+
+int score=100;
+  if (score >= 90) {
+    print("Excellent");
+  } else if (score >= 70) {
+    print("Good");
+ } else {
+    print("Needs" "Improvement");
+}
+
+
+String city="cairo";
+  if(city=="cairo"){
+    print("you are in Egypt");
+  }else{print("Unknown Location");}
+
+
+
+
+
+
+
+}
